5b9f7c9bd0918035f351cc3421fad401c823b6a4
alok2297 <alokkumar2297812@gmail.com>
2025-03-26 13:29:54 +0530

    Striver Sheet

---
+++ Striver180/01 - Set Matrix 0/Brute.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Brute{
+    Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        System.out.print("Striver 180\n");
+    }
+}
